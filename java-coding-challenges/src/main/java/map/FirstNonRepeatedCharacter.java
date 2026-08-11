5635ad2651d023b3024d3487b56bf49ddee0b288
Rachitha Kharvi <rachithakharvia@gmail.com>
2026-08-11 18:28:13 +0530

    added other two approach

--- java-coding-challenges/src/main/java/map/FirstNonRepeatedCharacter.java
+++ java-coding-challenges/src/main/java/map/FirstNonRepeatedCharacter.java
@@ -2,7 +2,9 @@ package map;
 
 import java.awt.image.ImageProducer;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class FirstNonRepeatedCharacter {
     public static void main(String args[]){
@@ -14,6 +16,18 @@ public class FirstNonRepeatedCharacter {
         Character firstNonrepeated=frequencyMap.entrySet().stream().filter(entry -> entry.getValue() == 1).
                 map(entry -> entry.getKey()).findFirst().orElse(null);
         System.out.println("\n"+firstNonrepeated);
-
+        // direct using of string
+        Character firstNonrepeatedChar= bankName.chars().mapToObj(c -> (char)c)
+                .filter(ch -> frequencyMap.get(ch) == 1).findFirst().orElse(null);
+        System.out.println(firstNonrepeatedChar);
+        //using set
+        Set<Character> set=new LinkedHashSet<Character>();
+        for(Character ch: bankName.toCharArray()){
+            if(!set.add(ch)){
+                set.remove(ch);
+            }
+        }
+        Character result=set.stream().findFirst().orElse(null);
+        System.out.println(result);
     }
 }
